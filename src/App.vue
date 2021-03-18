bef6448f57308341928c574407ba62cc17bf5df1
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2021-03-18 23:42:11 +0300

    add components, block and condition rendering

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,25 @@
 <template>
-  <h1>{{ title }}</h1>
-  <p>Welcome...</p>
-  <teleport to=".modals" v-if="showModal">
-    <Modal theme="" @close="toggleModal">
-      <template v-slot:links>
-        <a href="#">sign up now</a>
-        <a href="#">more info</a>
-      </template>
-      <h1>Ninja Giveaway!</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </Modal>
-  </teleport>
-
-  <teleport to=".modals" v-if="showModalTwo">
-    <Modal theme="sale" @close="toggleModalTwo">
-      <template v-slot:links>
-        <a href="#">sign in</a>
-        <a href="#">reboot</a>
-      </template>
-      <h1>Some different title!</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-    </Modal>
-  </teleport>
-
-  <button @click="toggleModal">open modal</button>
-  <button @click="toggleModalTwo">open second modal</button>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
-import Modal from "./components/Modal.vue";
+import Block from "@/components/Block";
 
 export default {
   name: 'App',
-  components: {
-    Modal
-  },
+  components: { Block },
   data() {
     return {
-      title: 'My First Vue App :))',
-      header: 'Sign up for the Giveaway!',
-      text: 'Grab your ninja swag for half price!',
-      showModal: false,
-      showModalTwo: false
+      isPlaying: false,
+      delay: null
     }
   },
   methods: {
-    toggleModal() {
-      this.showModal = !this.showModal
-    },
-    toggleModalTwo() {
-      this.showModalTwo = !this.showModalTwo
+    start() {
+      this.delay = 2000 + Math.random() * 5
+      this.isPlaying = true
     }
   }
 }
@@ -61,12 +31,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
-}
-h1 {
-  border-bottom: 1px solid #ddd;
-  display: inline-block;
-  padding-bottom: 10px;
 }
 </style>
