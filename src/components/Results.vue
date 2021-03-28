83aac72aa4ad321139399dac635f8dff1709239b
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2021-03-28 13:23:51 +0300

    add some styles and results rank data

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,15 +1,34 @@
 <template xmlns="http://www.w3.org/1999/html">
   <h2>Reaction time is</h2>
   <p>{{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
   name: "Results",
-  props: ['score']
+  props: ['score'],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Ninja fingers'
+    } else if (this.score < 400) {
+      this.rank = 'Rapid Reflexes'
+    } else {
+      this.rank = 'Snail pace...'
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+  }
 </style>
