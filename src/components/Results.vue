1572d7ec5fdea73df22346a46e868877a1d4f352
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2021-03-28 13:13:11 +0300

    add results to separate component

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,10 +1,12 @@
-<template>
-
+<template xmlns="http://www.w3.org/1999/html">
+  <h2>Reaction time is</h2>
+  <p>{{ score }} ms</p>
 </template>
 
 <script>
 export default {
-  name: "Results"
+  name: "Results",
+  props: ['score']
 }
 </script>
 
